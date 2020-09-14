54d4ed44b7a17c1f574c8c8aa746c2de25919dbd
Jose Mari De Jesus <jdejesus@buildzoom.com>
2020-09-14 21:50:43 +0800

    Finished backend of the price alerter

---
+++ src/models/alerts/constants.py
@@ -0,0 +1,5 @@
+URL = 'https://api.mailgun.net/v3/sandbox5593ce651a4c452bb25f4ef1512c934b.mailgun.org/messages'
+API_KEY = 'key-b41cc766321f03b2c117b8eaa910016b'
+FROM = 'Mailgun Sandbox <mailgun@sandbox5593ce651a4c452bb25f4ef1512c934b.mailgun.org>'
+ALERT_TIMEOUT = 1
+COLLECTION = 'alerts'
